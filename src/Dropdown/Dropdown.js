fc1137436fd325060712b0a1b9fb71da2765c953
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2022-01-21 19:48:57 +0200

    ounter, dropdown, colorpicker

---
+++ src/Dropdown/Dropdown.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./Dropdown.css";
+
+class Dropdown extends Component {
+  state = {
+    visible: false,
+  };
+
+  toggle = () => {
+    this.setState((prevState) => ({
+      visible: !prevState.visible,
+    }));
+  };
+  // show = () => {
+  //     this.setState({visible: true})
+  // }
+
+  // hide = () => {
+
+  //     this.setState({visible: false})
+  // }
+  render() {
+    return (
+      <div className="Dropdown">
+        <button type="button" onClick={this.toggle} className="Dropdown__togle">
+          {this.state.visible ? "скрыть" : "показать"}
+        </button>
+
+        {this.state.visible && (
+          <div className="Dropdown__menu">выпадающее меню</div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Counter.css";
+import Controls from "./Controllers";
+import Value from "./Value";
+class Counter extends React.Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+
+  state = {
+    value: this.props.initialValue,
+    a: 1,
+    b: 2,
+  };
+  handleIncrement = () => {
+    console.log("кликнули увеличить");
+    this.setState((prevState) => {
+      return {
+        value: prevState.value + 1,
+      };
+    });
+  };
+
+  currState = {
+    value: 12,
+    a: 1,
+    b: 2,
+  };
+
+  update = {
+    value: 44,
+  };
+
+  handleDecrement = () => {
+    console.log("кликнули уменьшить");
+    this.setState((prevState) => {
+      return {
+        value: prevState.value - 1,
+      };
+    });
+  };
+  render() {
+    return (
+      <div className="Counter">
+        <Value value={this.state.value} />
+
+        <Controls
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
+      </div>
+    );
+  }
+}
+
+export default Counter;
